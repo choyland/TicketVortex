99e0d408ac5a9a91f21d2af5644fa4ad138ea0fd
choyland <choyland@astech.co.uk>
2017-06-25 10:26:26 +0100

    initial commit

---
+++ TicketVortex.Cms/TicketVortex.Cms/UmbracoApplication.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+
+namespace TicketVortex.Cms
+{
+    public class UmbracoApplication : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+           
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register all controllers found in this assembly
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterControllers(typeof(Umbraco.Web.UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(Umbraco.Web.UmbracoApplication).Assembly);
+
+            // Add types to be resolved
+            RegisterTypes(builder, applicationContext);
+
+            // Configure Http and Controller Resolvers
+            var container = builder.Build();
+            System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private void RegisterTypes(ContainerBuilder builder, ApplicationContext context)
+        {
+            builder.RegisterInstance(context.Services.ContentService)
+                .As<IContentService>();
+        }
+    }
+}
---
+++ TicketVortex.Cms/TicketVortex.Cms/WebApi/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using TicketVortex.Cms.Model.Models;
+using Umbraco.Web.WebApi;
+
+namespace TicketVortex.Cms.WebApi
+{
+    public class EventsController : UmbracoApiController
+    {
+        public IEnumerable<Event> GetAllEvents()
+        {
+            return new List< Event>();
+        }
+
+        public Event GetEvent(int umbracoNodeId)
+        {
+            return new Event();
+        }
+
+        public IEnumerable<EventDate> GetEventDates(int eventUmbracoNodeId)
+        {
+            return new List<EventDate>();
+        }
+
+        public EventDate GetEventDate()
+        {
+            return new EventDate();
+        }
+    }
+}
